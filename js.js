c8f125f5cc3bc48093fd425f073d1035d07dc7aa
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2021-06-07 17:00:51 -0300

    refatoracoes e mostrar vencedor/empate

--- js.js
+++ js.js
@@ -11,57 +11,81 @@ const t7 = document.getElementById("t7");
 let cor = [];
 cor[0] = 1;
 
-let posicao = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]];
+let jogoAcabou = false;
+
+let posicao = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0]
+];
 
 let pos = 0;
 
 
 const vitoriaLinha = (arr) => {
+    //verificar se ocorreu vitoria no sentido horizontal
     const condicao = new RegExp('1111','g');
-
     let arrStr = arr.reduce((acc,el) => `${acc} ${el.join('')}`,'');
-    // console.log(arrStr);
     return condicao.test(arrStr);
 }
-vitoriaLinha(posicao);
 
 const vitoriaColuna = (arr) => {
+    //verificar se ocorreu vitoria no sentido vertical
     let newArr = [];
     for(let i = 0;i < 7;i++){
         newArr.push(arr.map((el) => el[i]));
     }
-    // console.log(newArr);
-
     return vitoriaLinha(newArr);
 }
 
 function deuEmpate(arr){
+    //verifica se preencheu tudo
     let newArr = [].concat(...arr)
     return !newArr.includes(0);
 }
 
 
-function criarBolinhas(t,cor,posicao,num){
-    if(t.childElementCount != 6){
-        if(cor[0] == 1){
-            let quadradox = document.createElement("div");
-            quadradox.className = "quadrado";
-            pos = t.childElementCount ;
-            posicao[5-pos][num] = 1;
-            t.appendChild(quadradox);
+function criarBolinhas(t,cor,posicao,indexColuna){
+    if(t.childElementCount !== 6 && jogoAcabou === false){
+        let bolinhaX = document.createElement("div");
+        pos = t.childElementCount ;
+        if(cor[0] === 1){
+            bolinhaX.className = "bolinhaJogador1";
+            posicao[5-pos][indexColuna] = 1;
+            if(vitoriaLinha(posicao) || vitoriaColuna(posicao)){
+                let vitoriaAlerta = document.createElement("p");
+                vitoriaAlerta.className = 'vitoria-alerta';
+                vitoriaAlerta.innerText = 'Jogador 1 venceu!!';
+                game.appendChild(vitoriaAlerta);
+                console.log('Jogador 1 venceu!!');
+                jogoAcabou = true;
+            }
             cor[0] = 0;
         }else{
-            let quadradox = document.createElement("div");
-            quadradox.className = "triangulo";
-            pos = t.childElementCount;
-            posicao[5-pos][num] = 2;
-            t.appendChild(quadradox);
+            bolinhaX.className = "bolinhaJogador2";
+            posicao[5-pos][indexColuna] = 2;
+            if(vitoriaLinha(posicao) || vitoriaColuna(posicao)){
+                let vitoriaAlerta = document.createElement("p");
+                vitoriaAlerta.className = 'vitoria-alerta';
+                vitoriaAlerta.innerText = 'Jogador 2 venceu!!';
+                game.appendChild(vitoriaAlerta);
+                console.log('Jogador 2 venceu!!');
+                jogoAcabou = true;
+            }
             cor[0] = 1;
         }
-
-        vitoriaColuna(posicao);
-        vitoriaLinha(posicao);
-
+        t.appendChild(bolinhaX);
+        if(deuEmpate(posicao)){
+            let empateAlerta = document.createElement("p");
+            empateAlerta.className = 'empate-alerta';
+            empateAlerta.innerText = 'Empate!!';
+            game.appendChild(empateAlerta);
+            console.log('Empate!!');
+            jogoAcabou = true;
+        }
     }
 }
 
