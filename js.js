287d87af62c0bec897b810f70e3abf54f37b8a49
Diogo Nava <diogonsdf@gmail.com>
2021-06-10 14:15:28 -0300

    Botão Kenzinho

--- js.js
+++ js.js
@@ -455,6 +455,7 @@ game.addEventListener("click",(e) => {
     game.style.pointerEvents='auto';
 });
 
+//Iniciar jogo indo para a tela de definir nomes para os jogadores
 const iniciar = document.getElementById("iniciar");
 const regras = document.getElementById("regras");
 const form = document.querySelector(".form");
@@ -465,6 +466,7 @@ iniciar.addEventListener("click", () => {
     somClick.play();
 });
 
+//Tela de nomes dos jogadores vai para o jogo
 pronto.addEventListener("click", () => {
     
     if (jogador1.value === "") {
@@ -479,7 +481,6 @@ pronto.addEventListener("click", () => {
         game.style.display = "flex";
         form.style.display = "none";
         placar.style.display = "flex";
-        pronto.style.display = "none";
         caixaTimer.style.display = "flex";
 
         for (let i = 0; i < hidden.length; i++) {
@@ -493,8 +494,8 @@ pronto.addEventListener("click", () => {
     }
 });
 
-let btn_reiniciar = document.getElementById("reiniciar");
-btn_reiniciar.addEventListener("click", reiniciar=()=>{
+//Inicia uma nova partida sem zerar o placar
+const reiniciar = () => {
     reiniciaTimer(timerValor,timerAtual);
     let pai = document.querySelectorAll('.torre');
     for(let i=0; i<pai.length; i++){
@@ -519,14 +520,34 @@ btn_reiniciar.addEventListener("click", reiniciar=()=>{
     }
 
     somClick.play();
-});
+};
 
+let btn_reiniciar = document.getElementById("reiniciar");
+btn_reiniciar.addEventListener("click", reiniciar);
+
+//Zera somente o placar
 let btn_zerar = document.getElementById("zerar");
 btn_zerar.addEventListener("click", zerar=()=>{
     placar1.innerText = Number('0');
     placar2.innerText = Number('0');
 
     somClick.play();
+});
+
+//Alterar os nomes dos jogadores
+let btn_trocarNomes = document.getElementById("trocar-nome");
+btn_trocarNomes.addEventListener("click", trocarNomes=()=> {
+    
+    game.style.display = "none";
+    form.style.display = "block";
+    placar.style.display = "none";
+    caixaTimer.style.display = "none";
+
+    for (let i = 0; i < hidden.length; i++) {
+        hidden[i].style.display = "none";
+    }
+
+    reiniciar();
 });
 
 
