5841a92b992522165ef3adfcbcab787f0b175fb0
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2021-06-10 21:15:43 -0300

    kenzinho sabe quando pode vencer em coluna

--- bot.js
+++ bot.js
@@ -3,8 +3,7 @@ const automatizarJogador2 = () => {
     reiniciaTimer(timerValor,timerAtual);
     let e = {};
     do{
-        let numeroTorre = torreAleatoria();
-        e.target = document.getElementById(`t${numeroTorre+1}`);
+        e.target = melhorColuna(posicao,ultimaJogadaKenzinho)
     }while(e.target.childElementCount === 6)
     cliqueJogador(e);
     game.addEventListener("click",cliqueJogador);
@@ -12,8 +11,21 @@ const automatizarJogador2 = () => {
 
 const torreAleatoria = () => Math.floor(Math.random()*7);
 
-// const colunaVitoriosa = (matriz,ultimaJogadaKenzinho) => {
-//     for(let i = ultimaJogadaKenzinho[0];i >= 0;i--){
-
-//     }
-// }
+const melhorColuna= (matriz,ultimaJogadaKenzinho) => {
+    const j = ultimaJogadaKenzinho[1];
+    let contador = 0;
+    for(let i = ultimaJogadaKenzinho[0];i < 6;i++){
+        if(i === 0){
+            break;
+        }
+        if(matriz[i][j] === 2){
+            contador++;
+        }
+    }
+    if(contador === 3){
+        return document.getElementById(`t${j+1}`);
+    } else {
+        let numeroTorre = torreAleatoria();
+        return document.getElementById(`t${numeroTorre+1}`);
+    }
+}
